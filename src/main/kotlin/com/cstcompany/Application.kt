fbee07d96113f4dde4aa434b1e09fa4c23cff046
Patrik Csikós <61376206+pacsinta@users.noreply.github.com>
2022-06-21 21:23:43 +0200

    init

---
+++ src/main/kotlin/com/cstcompany/Application.kt
@@ -0,0 +1,12 @@
+package com.cstcompany
+
+import io.ktor.server.engine.*
+import io.ktor.server.netty.*
+import com.cstcompany.plugins.*
+
+fun main() {
+    embeddedServer(Netty, port = 8080, host = "0.0.0.0") {
+        configureRouting()
+        configureSerialization()
+    }.start(wait = true)
+}
